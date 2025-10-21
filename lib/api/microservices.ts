7a5b7b001fa7f617ec74b381c4a74b6533ae3315
tirenton <it9026@gmail.com>
2025-10-22 01:37:17 +0700

    fix route

--- lib/api/microservices.ts
+++ lib/api/microservices.ts
@@ -1,2 +1,4 @@
 export const BASE_URL = "http://localhost:3000";
 export const USERS_URL = "users-service";
+export const INVENTORY_URL = "inventory-service";
+export const DELIVERIES_URL = "deliveries-service";
